a24adc4e14814d2cc1a8f6f926b090ad0ca53512
asit <asit@asits.(none)>
2015-10-07 19:23:19 +0530

    seg fault and wrong method to calculate sum_closest to zero finding index is fine

--- index_max_subarray.cpp
+++ index_max_subarray.cpp
@@ -7,6 +7,13 @@ struct point {
 	int last;
 };
 
+int modulu(int n)
+{
+	if(n < 0)
+		return -1 * n;
+	return n;
+}
+//finding indices.
 struct point* max_sum_indices(int arr[], int n)
 {
 	int sum_current = 0;
@@ -31,12 +38,56 @@ struct point* max_sum_indices(int arr[], int n)
 	return indices;
 }
 
+//finding sum closest to zero.
+
+struct point* sum_closest_to_zero(int arr[], int n)
+{
+	map <int, int> m;
+	int sum_current = 0;
+	int sum_close = 10000;
+	struct point *d;
+	m.insert(arr[0]) = 0;
+	for(int i =1; i < n; i++)
+	{
+		m.insert(make_pair(+arr[i],i));
+	}
+	int diff_arr[8],ind_arr[9];
+	int temp=0;
+	int k = 0,j=0;
+	map <int, int> :: iterator it = m.begin();
+	while(it != m.end())
+	{
+		ind_arr[j++] = it->second;
+		if(temp != 0)
+		{
+			arr[k++] = it->first - temp;
+		}
+		temp = it->first;
+		++it;
+	}
+	int min = 999;
+	int min_index;
+	for(int i = 0; i <8; i++)
+	{
+		if(modulu(arr[i]) < modulu(min))
+		{
+			min = arr[i];
+			min_index = i;
+		}
+	}
+	d->first = ind_arr[min_index];
+	d->last = ind_arr[min_index+1];
+	return d;
+}
+
 int main()
 {
 //	int arr[8] = {-3,4,-5,6,9,-2,-3,1};
 	int arr1[9] = {-2,1,-3,4,-1,2,1,-5,4};
-	struct point *indices;
-	indices = max_sum_indices(arr1,9);
-	cout << indices->first << "-" << indices->last << endl;
+	struct point *indices, *indices1;
+	//indices = max_sum_indices(arr1,9);
+	indices1 = sum_closest_to_zero(arr1,9);
+	//cout << indices->first << "-" << indices->last << endl;
+	cout << indices1->first << "-" << indices1->last << endl;	
 	return 0;
 }
